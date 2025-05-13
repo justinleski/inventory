be448fceb00b2485ea447ab6a705bbaf7706a435
Justin <o0justinsgamez0o@gmail.com>
2025-05-13 19:13:02 -0400

    fix: Ensure proper port chosen

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
